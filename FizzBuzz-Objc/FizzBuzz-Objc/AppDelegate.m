97a8eb00805a30bf4c33a078f3f8ba74f7763a5c
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-02-02 01:48:01 -0700

    Completed FizzBuzz
    PreCourse Project 6

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -12,10 +12,49 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    // Insert code here
-    
+    for (int i = 1; i <= 100; i++) {
+        if ([self isBuzzed:i] == true && [self isFizzed:i] == false) {
+            NSLog(@"Buzz");
+        }
+        
+        if([self isBuzzed:i] == false && [self isFizzed:i] == true) {
+            NSLog(@"Fizz");
+        }
+        
+        if([self isBuzzed:i] == true && [self isFizzed:i] == true) {
+            NSLog(@"FizzBuzz");
+        }
+
+        if([self isBuzzed:i] == false && [self isFizzed:i] == false) {
+            NSLog(@"%d", i);
+        }
+    }
     return YES;
+}
+
+-(BOOL)isBuzzed:(int)number{
+    NSString *numToString = [@(number) stringValue];
+    if ((number % 3) == 0) {
+        return true;
+    }else if ([numToString rangeOfString:@"3"].location != NSNotFound){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+-(BOOL)isFizzed:(int)number{
+    NSString *numToString = [@(number) stringValue];
+    if ((number % 5) == 0) {
+        //NSLog(@"divisible by 5");
+        return true;
+    }else if ([numToString rangeOfString:@"5"].location != NSNotFound){
+        //NSLog(@"contains 5");
+        return true;
+    }else{
+        // NSLog(@"Is not Fizzed");
+        return false;
+    }
 }
 
 @end
